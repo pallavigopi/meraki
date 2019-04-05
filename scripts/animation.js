c5de0cc5b31eac7063e2a8e5eb07b66b777a7fef
nehamj <nehamj798@gmail.com>
2019-04-05 19:31:00 +0530

    New Landing page

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,48 +1,70 @@
 $(document).ready(function(){
-   let i,circle=[];
-          for(i=0;i<300;i+=1){
-              circle[i] = $('<div id="d'+i+'"><div id="c'+i+'" class="wing"></div></div>'); 
-              $('#butterfly').append(circle[i]);
-              $("#d"+i).css({"animation":"fly 5s linear "+i/100+"s 1",
-                             "margin": i+"px 0px 0px "+ (Math.floor(Math.random() * 1000)) +"px",
-                             "opacity":"0"
-                           });
-              $("#c"+i).css({"animation": "wings 0.5s linear "+i/100+"s infinite",});
-          }
-  setTimeout(del, 8000);
- function del() { circle=[]; }
+  setTimeout(citybw, 11000);
+ function citybw() { $("#citybw").css({"visibility":"visible"}); } 
+  setTimeout(cityzoom, 13000);
+ function cityzoom() { $("#cityzoom").css({"visibility":"visible"}); } 
+  setTimeout(city, 13100);
+ function city() { $("#city").css({"visibility":"visible"}); } 
+  setTimeout(citymzoom, 15000);
+ function citymzoom() { $("#citymzoom").css({"visibility":"visible"}); } 
+  setTimeout(citym, 15100);
+ function citym() { $("#citym").css({"visibility":"visible"}); }  
+
+   setTimeout(text, 12000);
+ function text() { $(".text").css({"color":"white"}); }  
+
+  setTimeout(citym1, 17000);
+ function citym1() { $("#citym1").css({"visibility":"visible"}); } 
+  setTimeout(citym2, 17000);
+ function citym2() { $("#citym2").css({"visibility":"visible"}); } 
+  setTimeout(citym3, 17000);
+ function citym3() { $("#citym3").css({"visibility":"visible"}); } 
+  setTimeout(citym4, 17000);
+ function citym4() { $("#citym4").css({"visibility":"visible"}); } 
+
+   setTimeout(dot1,5000);
+ function dot1() { $(".dot").css({"visibility":"visible"}); } 
+   setTimeout(dot2,10000);
+ function dot2() { $(".dot").css({"visibility":"hidden"}); } 
+    setTimeout(box1,7000);
+ function box1() { $("#box").css({"visibility":"visible"}); } 
+   setTimeout(box2,10000);
+ function box2() { $("#box").css({"visibility":"hidden"}); } 
+
+    setTimeout(butterfly,5000);
+ function butterfly() { $("#butterfly").css({"visibility":"hidden"}); } 
+
+
+
   setTimeout(one1, 0);
  function one1() { $("#one").css({"visibility":"visible"}); }
-  setTimeout(one2, 8000);
+  setTimeout(one2, 5000);
  function one2() { $("#one").css({"visibility":"hidden"}); }
-  setTimeout(butterfly1, 0);
- function butterfly1() { $("#butterfly").css({"visibility":"visible"}); }
-  setTimeout(butterfly2, 8000);
- function butterfly2() { $("#butterfly").css({"visibility":"hidden"}); }
-  setTimeout(two1,8000);
+  setTimeout(two1,5000);
  function two1() { $("#two").css({"visibility":"visible"}); }
-  setTimeout(two2, 13000);
+  setTimeout(two2, 10000);
  function two2() { $("#two").css({"visibility":"hidden"}); }
-  setTimeout(canvas1,8000);
- function canvas1() { $("#canvas").css({"visibility":"visible"}); }
-  setTimeout(canvas2,13000);
- function canvas2() { $("#canvas").css({"visibility":"hidden"}); }
-  setTimeout(three1,13000);
+  setTimeout(three1,10000);
  function three1() { $("#three").css({"visibility":"visible"}); }
-  setTimeout(three2, 15000);
+  setTimeout(three2, 12000);
  function three2() { $("#three").css({"visibility":"hidden"}); }
-  setTimeout(four1, 15000);
+  setTimeout(four1, 12000);
  function four1() { $("#four").css({"visibility":"visible"}); }
-  setTimeout(four2, 20000);
+  setTimeout(four2, 17000);
  function four2() { $("#four").css({"visibility":"hidden"}); }
-  setTimeout(ripple1,15000);
- function ripple1() { $("#ripple").css({"visibility":"visible"}); }
-  setTimeout(ripple2, 20000);
- function ripple2() { $("#ripple").css({"visibility":"hidden"}); }
-  setTimeout(five1, 20000);
+  setTimeout(five1, 17000);
  function five1() { $("#five").css({"visibility":"visible"}); }
-  setTimeout(five2, 25000);
+  setTimeout(five2, 23000);
  function five2() { $("#five").css({"visibility":"hidden"}); }
-   setTimeout(logos, 25000);
+   setTimeout(logos, 23000);
  function logos() { $(".logo").css({"visibility":"visible"}); } 
+
+  
+
+
+
+
+
+
+
 });
